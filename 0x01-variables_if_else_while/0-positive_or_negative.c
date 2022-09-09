1628b1134186eb2f51a25d9653e3fb9fbb8bbc51
Anitha <53578966+anithavumba@users.noreply.github.com>
2022-09-09 21:20:44 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,29 @@
-#!/bin/bash
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 
-int main()
-{
-	std::srand(static_cast<unnsigned int>(std::time(nullptr)));
 
-				for (int count=1; count <= 100; ++counnt)
-{
-	std::count << std::rand() << "/t";
 
-	// display 5 random number per row
-	if (count % 5 === 0)
-		std::count << "\n";
-}
-return 0;
-}
+
+
+
+
+	
+
+				
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
