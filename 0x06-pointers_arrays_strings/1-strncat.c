0de6d72e6788d5228fcb3f8e9c53c58690a9f900
anithavumba <anithavumba@gmail.com>
2022-09-21 11:56:00 -0700

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatinates two strings dest and src, then n is the size of src
- * @dest: pointer to dest array 
- * @src: string to be appended
- * @n: maximum number of characters to be appended
- *Return: Always 0
+ *_strncat - function to concatenate two strings and return a new string
+ *with n elements of the appended string
+ *@dest: string 1
+ *@src: string 2
+ *@n: number of chars to include from appended string
+ *Return: char pointer
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char src[];
-	char dest[];
-	char *ptr;
+	int i = 0, j = 0;
+	char *p;
 
-	putchar("%s\n", src);
-	putchar("%s\n", dest);
-
-	ptr = _strncat(dest, src, n);
-
-	putchar("%s", ptr);
-
-		return (0)
+	while (*(dest + i) != '\0')
+		i++;
+	while (!(*(src + j) == '\0' || j == n))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+	p = dest;
+	return (p);
 }
-
