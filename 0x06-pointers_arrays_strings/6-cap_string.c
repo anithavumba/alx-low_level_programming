b32ed251449dba17edd0c57974605d4278069add
anithavumba <anithavumba@gmail.com>
2022-09-21 13:38:50 -0700

    av

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
 /**
-
- * cap_string - capitalizes all words of a string
-
- * @s: input string.
-
- * Return: the pointer to dest.
-
- */
+* cap_string - capitalizes all words of a string
+* @s: input string.
+* Return: the pointer to dest.
+*/
 
 char *cap_string(char *s)
 
