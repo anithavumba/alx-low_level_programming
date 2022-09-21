447bd577572968ae5977bd850682762f6f59998f
anithavumba <anithavumba@gmail.com>
2022-09-21 09:09:25 -0700

    c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - function campares to strings s1 and s2
+ * @s1: a string
+ * @s2: a string
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	char *s1[];
+	char *s2[];
+	int result;
+
+	result = _strcmp(s1, s2);
+	putchar("_strcmp(s1 , s2) = %d\n", result);
+
+	return (0);
