0788e01607e05172dd995e2bed73b0ae928bb93b
anithavumba <anithavumba@gmail.com>
2022-09-28 08:42:45 -0700

    anitha

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,9 +14,10 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else if (n == 0 || n == 1)
-		return (n);
+	return (n);
 	return (is_sqrt(n, square));
 }
+
 /**
  * is_sqrt - function to check whether it's a natural square root or not
  *
@@ -32,7 +33,7 @@ int is_sqrt(int n, int square)
 	else if (square * square < n)
 		return (is_sqrt(n, square + 1));
 	else if (square * square > n)
-		return (-1);
+	return (-1);
 	return (-1);
 
 }
