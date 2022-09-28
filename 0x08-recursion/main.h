4e8bcb80b05cca1de6f6a98af905acfde83f9fbe
anithavumba <anithavumba@gmail.com>
2022-09-28 11:23:47 -0700

    anitha

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int is_prime(int n, int i);
+int is_sqrt(int n, int square);
+int palindrome_check(char *s, int len, int i);
 
 #endif
