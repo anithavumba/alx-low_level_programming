ec55a723b19333f3d4f5f4a0a473ea99d6230300
anithavumba <anithavumba@gmail.com>
2022-10-03 08:52:00 -0700

    anita

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - main entry
+ * @side: input size
+ * @c: char
+ * Return: Always 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	array = malloc(sizeof(char) * size);
+
+	if (size == 0 || array == NULL)
+		return (NULL); 
