bc7a0dc4ce9c46908d6fb9be972533f26345527c
anithavumba <anithavumba@gmail.com>
2022-10-27 19:15:19 +0200

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
  *Return: if a error occur return -1.
  * otherwise return 1.
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
