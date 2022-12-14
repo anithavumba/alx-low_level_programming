0c443982f826f693ab2f16c83a6d1f1358185533
anithavumba <anithavumba@gmail.com>
2022-12-14 21:43:13 +0200

    anita

---
+++ 0x18-dynamic_libraries/hacked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ *printf - prints out output
+ * in standard output
+ *@format: poiter
+ *Return: SUCCESS Always
+ */
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
+}
+
