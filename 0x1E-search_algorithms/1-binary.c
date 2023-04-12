dcfe63cb905a2a85f974937c237a26edc70b8d59
anithavumba <anithavumba@gmail.com>
2023-04-12 15:37:37 +0200

    update the on search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,61 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
  *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
  * Return: index of the number
  */
-int jump_search(int *array, size_t size, int value)
+int recursive_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
+	size_t half = size / 2;
+	size_t i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
+	printf("Searching in array");
 
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
+	printf("\n");
 
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+	if (half && size % 2 == 0)
+		half--;
 
-	for (; prev <= index && prev < (int)size; prev++)
-	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
-	}
+	if (value == array[half])
+		return ((int)half);
 
-	return (-1);
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
